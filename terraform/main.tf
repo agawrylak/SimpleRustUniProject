131b6355af0a3ed451fccf441dcc6e25f47754f8
agawrylak <artur.gawrylak@o2.pl>
2023-06-11 00:58:18 +0200

    Added deploy job based on terraform to push code to beanstalk

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_elastic_beanstalk_application" "ag-zad2" {
+  name = "ag_zad_2"
+}
+
+resource "aws_elastic_beanstalk_environment" "ag-zad2-env" {
+  name                = "ag-zad2-env"
+  application         = aws_elastic_beanstalk_application.ag-zad2.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.5.8 running Docker"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "terraform-aws"
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "eu-central-1"
+}
